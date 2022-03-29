6f1c0aff9cde8a2cd5b5763681cef8aa4d63309d
Jarred Parr <jparr721@gmail.com>
2022-03-29 12:15:59 -0600

    add linear shape functions

--- src/fem/phi_linear_tetrahedron.cc
+++ src/fem/phi_linear_tetrahedron.cc
@@ -1,1 +1,27 @@
 #include "phi_linear_tetrahedron.h"
+
+namespace nm::fem {
+    void phi_linear_tetrahedron(const nm::matXr &vertices, const vec4i &tet, const nm::vec3r &ref, nm::vec4r &phi) {
+        const auto X0 = vertices.row(tet(0));
+        const auto X1 = vertices.row(tet(1));
+        const auto X2 = vertices.row(tet(2));
+        const auto X3 = vertices.row(tet(3));
+
+        // The matrix T represents the edge vectors from our source point X0 to each of the other X points in reference
+        // space
+        mat3r T;
+        T.col(0) = X1 - X0;
+        T.col(1) = X2 - X0;
+        T.col(2) = X3 - X0;
+
+        // Now, we must apply our reference coordinate difference to get the other side of the matrix function.
+        vec3r b = ref - X0;
+
+        // Now, we can solve the linear system. As Ax=b where A is T and phi is our basis function mapping.
+        phi.segment<3>(1) = T.fullPivHouseholderQr().solve(b); // phi_1, 2, 3
+
+        // phi_0 is a linear combination of its elements.
+        phi(0) = 1 - phi(1) - phi(2) - phi(3);
+    }
+
+}// namespace nm::fem
--- src/fem/phi_linear_tetrahedron.h
+++ src/fem/phi_linear_tetrahedron.h
@@ -1,8 +1,22 @@
-//
-// Created by Jarred Parr on 3/29/22.
-//
+#pragma once
 
-#ifndef NIGHTMARE_PHI_LINEAR_TETRAHEDRON_H
-#define NIGHTMARE_PHI_LINEAR_TETRAHEDRON_H
+#include "../nm_math.h"
 
-#endif//NIGHTMARE_PHI_LINEAR_TETRAHEDRON_H
+namespace nm::fem {
+    /**
+     * \brief Computes the mapping between reference space coordinates into the deformed space coordinates. For this, we
+     * need to construct a linear system where we know A and b, but not x. In this case, we have something like:
+     *
+     * b = [X Y Z] (World Space Coordinates)\n
+     * A = [X0 X1 X2 X3] (Matrix of X0, Y0, Z0, ... triplets)\n
+     * x = Unknown (represents our matrix of shape functions for each point in our tetrahedral.\n
+     *
+     * We can then stack these together and solve the linear system to get the basis functions out.
+     *
+     * @param vertices The matrix of vertex positions in our entire simulated object.
+     * @param tet The specific tetrahedral elements we are interested in accessing.
+     * @param ref The reference space coordinates on which to compute.
+     * @param phi The 4x1 values of the basis functions for phi.
+     */
+    void phi_linear_tetrahedron(const matXr &vertices, const vec4i &tet, const vec3r &ref, vec4r &phi);
+}
