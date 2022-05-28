82281be8ab2fa1f17d9da684dc9a2374a29c7b46
Jarred Parr <jparr721@gmail.com>
2022-05-27 21:08:40 -0600

    attribution

--- src/hyperelastic/neo_hookean.cc
+++ src/hyperelastic/neo_hookean.cc
@@ -6,15 +6,17 @@ namespace nm::hyperelastic {
         const real logJ = std::log(J);
 
         // Invariant 2
+        // http://www.tkim.graphics/DYNAMIC_DEFORMABLES/DynamicDeformables.pdf
+        // Appendix B
         const real I2 = doubleContraction(F, F);
-        return mu * 0.5 * (I2 - 3) - mu * logJ + lambda * 0.5 * logJ * logJ;
+        return mu * 0.5 * (I2 - 3.0) - mu * logJ + lambda * 0.5 * logJ * logJ;
     }
 
     auto pk1(const mat3 &F, real lambda, real mu) -> mat3 {
         const real J = F.determinant();
         const real logJ = std::log(J);
         const mat3 gradJ = identities::gradientJ(F);
-        return mu * (F - (1 / J) * gradJ) + lambda * (logJ / J) * gradJ;
+        return mu * (F - (1.0 / J) * gradJ) + lambda * (logJ / J) * gradJ;
     }
 
     auto dpk1() -> mat9 { return mat9(); }
